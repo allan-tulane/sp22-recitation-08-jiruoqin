c6eb7986c780bfda2dc8d9bb4578c67cfc69a78e
Ruoqin <55730329+jiruoqin@users.noreply.github.com>
2022-04-27 15:40:15 -0500

    done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from cgitb import reset
 from collections import defaultdict
+from functools import reduce 
 
 def make_undirected_graph(edge_list):
     """ Makes an undirected graph from a list of edge tuples. """
@@ -14,12 +16,20 @@ def reachable(graph, start_node):
     Returns:
       the set of nodes reachable from start_node
     """
-    result = set([start_node])
+    #result = set([start_node])
     frontier = set([start_node])
-    while len(frontier) != 0:
-        ###TODO
-        pass
-    return result
+    visited = set()
+    def reach(visited, frontier):
+        if len(frontier) == 0:
+            return visited 
+        else:
+            visited_new = visited | frontier
+            visited = visited_new
+            frontier_neighbors = reduce(set.union, [graph[f] for f in frontier])
+            frontier = frontier_neighbors - visited
+        return reach(visited, frontier)
+        
+    return reach(visited, frontier)
 
 def test_reachable():
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
@@ -33,8 +43,14 @@ def test_reachable():
 
 
 def connected(graph):
-    ### TODO
-    pass
+    start_ndoe = next(iter(graph))
+    reachable_set = reachable(graph, start_ndoe)
+    all_nodes = graph.keys()
+    for n in all_nodes:
+        if n not in reachable_set:
+            return False
+    
+    return True 
 
 def test_connected():
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
@@ -49,8 +65,15 @@ def n_components(graph):
     Returns:
       the number of connected components in an undirected graph
     """
-    ### TODO
-    pass
+    all_nodes = list(graph.keys())
+    count = 0
+    while len(all_nodes) != 0:
+        start_ndoe = all_nodes[0]
+        reachable_set = reachable(graph, start_ndoe)
+        count += 1
+        all_nodes = list(set(all_nodes).difference(reachable_set))
+    
+    return count 
 
 def test_n_components():
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
